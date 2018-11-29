47bcacbf08a29997c701de3532fb9e6da5a6be90
MisterSmithe <33697062+MisterSmithe@users.noreply.github.com>
2018-11-29 14:20:46 -0500

    Update HelloWorld.java
    Uses a Scanner instead of a BufferedReader (I'm really just playing around in GitHub)

--- java/child/HelloWorld.java
+++ java/child/HelloWorld.java
@@ -1,15 +1,13 @@
 //this is a test program to see if all is well
 //place code in Test Activity -> Edit
 
-import java.io.*;
+import java.util.Scanner;
 
 public class HelloWorld {
-  public static void main(String[] args) throws IOException {
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    String s;
-    while ((s = in.readLine()) != null && s.length() != 0)
-      System.out.println(s);
-      System.out.println("Remove me to pass the test.");
-    // An empty line or Ctrl-Z terminates the program
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String s = in.nextLine();
+    System.out.println(s);
+    System.out.println("Remove me to pass the test.");
   }
 }
